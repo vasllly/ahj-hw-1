13be1964003fa51050ddb53ff33ed566b9a36b33
Ganja Vasiliy <vasllly219@gmail.com>
2019-04-30 00:11:59 +0300

    FIX favicon

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html',
+      favicon: './src/favicon.ico',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
